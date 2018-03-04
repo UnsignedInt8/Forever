4c12e78e7d023d2371393c7c4109f54b16a9c0fd
UnsignedInt8 <unsignedint8@gmail.com>
2018-03-04 15:33:47 +0800

    progressive image loading

--- src/components/ImagePreview.tsx
+++ src/components/ImagePreview.tsx
@@ -1,15 +1,35 @@
 import * as React from 'react';
+import ProgressiveImage from 'react-progressive-image';
+import { Spin, Row, Icon } from 'antd';
 
 interface ImageProps {
     ipfsHash?: string;
     title?: string;
 }
 
-export default class ImagePreview extends React.Component<ImageProps, {}>{
+interface ImageStates {
+    isLoading: boolean;
+}
+
+export default class ImagePreview extends React.Component<ImageProps, ImageStates>{
+
+    constructor(props: ImageProps, ctx?: any) {
+        super(props, ctx);
+        this.state = { isLoading: true };
+    }
+
+    componentWillReceiveProps() {
+        this.setState({ isLoading: true });
+    }
+
     render() {
         return (
-            <div style={{ paddingTop: 12 }}>
-                <img style={{ width: '100%', }} src={`https://ipfs.io/ipfs/${this.props.ipfsHash}`} alt={this.props.title} />
+            <div style={{ paddingTop: 12, position: 'relative', textAlign: 'center' }}>
+                <Icon type="loading" style={{ fontSize: 24, color: 'deepskyblue', display: `${this.state.isLoading ? 'inline-block' : 'none'}` }} spin />
+
+                <ProgressiveImage src={`https://ipfs.io/ipfs/${this.props.ipfsHash}`} placeholder='' >
+                    {(src) => <img style={{ width: '100%' }} src={src} alt='' onLoad={e => this.setState({ isLoading: false })} />}
+                </ProgressiveImage>
             </div>
         );
     }
